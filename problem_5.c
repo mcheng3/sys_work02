f81ff4f004b2737e04bd7624551af5f36b4e48b7
mcheng3 <michael.cheng65@gmail.com>
2017-09-15 11:48:56 -0400

    Add problem_5.c

---
+++ problem_5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int count;
+	int x = 1;
+	int solved;
+	
+	while(solved != 20){
+		for(count = 1; count <= 20; count++){
+			if(x % count = 0){
+				solved++;
+				}
+				
+				
