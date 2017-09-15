5a993d797c1947f5d30637ac5c834f345925ae64
mcheng3 <michael.cheng65@gmail.com>
2017-09-15 11:46:36 -0400

    Add problem_1.c

---
+++ problem_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){	
+	int i;
+	int sum = 0;
+	for(i = 1; i < 1000; i++){
+		if(i % 3 == 0 || i % 5 == 0){
+			sum += i;
+		}
+	}
+	
+	printf("%d\n", sum);
+}
+			
