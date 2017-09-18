a383ee29bdc8bd95132471808118affa8f74a5b6
mcheng3 <michael.cheng65@gmail.com>
2017-09-17 21:40:34 -0400

    Finish problem_5.c

--- problem_5.c
+++ problem_5.c
@@ -4,12 +4,20 @@
 int main(){
 	int count;
 	int x = 1;
-	int solved;
+	int solved = 0;
 	
 	while(solved != 20){
+		solved = 0;
 		for(count = 1; count <= 20; count++){
-			if(x % count = 0){
+			if(x % count == 0){
 				solved++;
-				}
+			}
+			else{
+				x++;
+			}
+		}
+	}
+	printf("%d\n", x);
+}
 				
 				
